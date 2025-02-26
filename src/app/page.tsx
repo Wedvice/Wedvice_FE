f5564955ad7772a2da1ebc9c37ac5a0132b30277
seonghyuk1 <tjdgur0737@naver.com>
2025-02-26 19:58:51 +0900

    [refactor] 스플래시 화면 변경
    - 전체 backGround svg -> 로고 svg 삽입
    - 문구 텍스트 변경
    - 그라데이션 컴포넌트 삽입 (임시 - 디자인/기획 답변 대기 중)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,27 @@ import LoginButton from '@/features/components/LoginButton';
 
 export default function Home() {
   return (
-    <div className='relative w-full h-screen bg-black'>
-      <Image
-        src='/wedy_splash.png'
-        alt='wedy_splash'
-        layout='fill'
-        objectFit='cover'
-        priority
-      />
+    <div className='relative flex h-screen w-full flex-col items-center justify-center overflow-hidden bg-black'>
+      <div className='absolute left-[-30px] top-[-50px] h-[200px] w-[200px] rounded-full bg-gradient-to-r from-purple-700 via-purple-500 to-transparent opacity-50 blur-3xl'></div>
+      <div className='absolute bottom-[-30px] right-[-50px] h-[250px] w-[250px] rounded-full bg-gradient-to-r from-purple-700 via-purple-500 to-transparent opacity-40 blur-3xl'></div>
 
-      <div className='absolute bottom-10 left-0 w-full flex justify-center px-6'>
+      <div className='absolute top-1/3 flex flex-col items-center'>
+        <Image
+          src='/wedySplashLogo.png'
+          alt='Wedy Logo'
+          width={135}
+          height={55}
+          priority
+        />
+        <h2 className='mt-4 w-[350px] text-center text-[24px] font-semibold leading-[31.2px] tracking-[-0.025em] text-white'>
+          결혼준비 레디? 웨디!
+        </h2>
+        <p className='mt-2 text-center text-[12px] font-medium leading-[15.6px] tracking-[-0.025em] text-gray-400'>
+          한 곳에 쏙, 모든 결혼 준비를 한눈에
+        </p>
+      </div>
+
+      <div className='absolute bottom-10 left-0 flex w-full justify-center px-6'>
         <LoginButton />
       </div>
     </div>
