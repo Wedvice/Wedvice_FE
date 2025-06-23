d4e2c588b6c7429633bf5e5e0b47bf6f1b377bb1
suji8073 <suzy8073@naver.com>
2025-06-23 17:03:18 +0900

    [refactor] TopList 컴포넌트에 onClick 및 disabled props 추가 - disabled 시 TopList의 opacity-50 style 적용

--- src/components/molecules/topList/TopList.tsx
+++ src/components/molecules/topList/TopList.tsx
@@ -4,12 +4,23 @@ interface TopListProps {
   label: string;
   currentCount: number;
   totalCount: number;
+  onClick: () => void;
+  disabled?: boolean;
 }
 
-export const TopList = ({ label, currentCount, totalCount }: TopListProps) => {
+export const TopList = ({
+  label,
+  currentCount,
+  totalCount,
+  disabled = false,
+  onClick,
+}: TopListProps) => {
   const isCompleted = currentCount === totalCount;
   return (
-    <div className='relative h-[171px] w-[171px]'>
+    <div
+      className={`${disabled ? 'opacity-50' : 'opacity-100'} relative h-[171px] w-[171px] cursor-pointer transition-opacity duration-200`}
+      onClick={onClick}
+    >
       <div
         className={`absolute right-0 top-[7px] flex h-[42px] w-[67px] justify-center rounded-tr-[8px] pt-[3px] ${
           isCompleted ? 'bg-primary-400' : 'bg-gray-300'
