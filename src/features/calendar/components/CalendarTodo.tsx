08d5f90165674edcbc8f01249f7998de7bc34396
suji8073 <suzy8073@naver.com>
2025-06-27 21:40:01 +0900

    [feat] CalendarTodo 컴포넌트 제작 - 캘린더 일정 항목 표시용 UI 컴포넌트 구현 - Type(예신, 예랑, 함께)별 barColod 출력

---
+++ src/features/calendar/components/CalendarTodo.tsx
@@ -0,0 +1,29 @@
+export type TodoType = '예신' | '예랑' | '함께';
+
+const colorMap: Record<TodoType, string> = {
+  예신: 'bg-primary-300',
+  예랑: 'bg-pink',
+  함께: 'bg-blue',
+};
+
+interface CalendarTodoProps {
+  title: string;
+  category: string;
+  type: TodoType;
+}
+
+export const CalendarTodo = ({ title, category, type }: CalendarTodoProps) => {
+  const barColor = colorMap[type];
+  return (
+    <div className='relative flex h-[77px] w-full items-center gap-3 rounded-lg bg-gray-100 p-4'>
+      <span
+        className={`absolute left-0 h-[45px] w-1 rounded-[4.5px] ${barColor}`}
+        aria-hidden='true'
+      />
+      <div className='flex h-full flex-1 flex-col justify-between font-medium'>
+        <h4 className='text-base text-gray-900'>{title}</h4>
+        <p className='text-xs text-gray-700'>{category}</p>
+      </div>
+    </div>
+  );
+};
