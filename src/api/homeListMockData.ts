2a875b8b8ea8ab8d8f8bc92928cfaf4f2fb1886a
vvizi <gmlwl70@gmail.com>
2025-07-21 22:33:58 +0900

    [feat] home list 용 Mock data 추가
    - data fetch 전 임시 데이터

---
+++ src/api/homeListMockData.ts
@@ -0,0 +1,27 @@
+import { TabType } from '@/components/molecules/homeList/HomeListTab';
+
+export type ListItem = { title: string; date: string };
+
+export type TabListData = {
+  todo?: ListItem[];
+  done?: ListItem[];
+};
+
+export const mockData: Record<TabType, TabListData> = {
+  together: {
+    todo: [
+      { title: '함께 준비할 리스트 1', date: '2025.07.21' },
+      { title: '함께 준비할 리스트 2', date: '2025.07.21' },
+      { title: '함께 준비할 리스트 3', date: '2025.07.21' },
+      { title: '함께 준비할 리스트 4', date: '2025.07.21' },
+    ],
+    done: [{ title: '함께 완료한 리스트', date: '2025.07.22' }],
+  },
+  bride: {
+    todo: [{ title: '예신 준비할 리스트', date: '2025.07.22' }],
+    done: [{ title: '예신 완료한 리스트', date: '2025.07.22' }],
+  },
+  groom: {
+    done: [{ title: '예랑 완료한 리스트', date: '2025.07.23' }],
+  },
+};
