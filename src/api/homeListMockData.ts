f6675d40b5aafadf3a445434ddfc80d3562c2608
vvizi <gmlwl70@gmail.com>
2025-07-22 21:21:52 +0900

    [refactor] mock data 수정

--- src/api/homeListMockData.ts
+++ src/api/homeListMockData.ts
@@ -11,15 +11,19 @@ export const mockData: Record<TabType, TabListData> = {
   together: {
     todo: [
       { title: '함께 준비할 리스트 1', date: '2025.07.21' },
-      { title: '함께 준비할 리스트 2', date: '2025.07.21' },
-      { title: '함께 준비할 리스트 3', date: '2025.07.21' },
-      { title: '함께 준비할 리스트 4', date: '2025.07.21' },
+      { title: '함께 준비할 리스트 2', date: '2025.07.24' },
+      { title: '함께 준비할 리스트 3', date: '2025.07.23' },
+      { title: '함께 준비할 리스트 4', date: '2025.07.10' },
     ],
     done: [{ title: '함께 완료한 리스트', date: '2025.07.22' }],
   },
   bride: {
-    todo: [{ title: '예신 준비할 리스트', date: '2025.07.22' }],
-    done: [{ title: '예신 완료한 리스트', date: '2025.07.22' }],
+    todo: [
+      { title: '예신 준비할 리스트 1', date: '2025.07.21' },
+      { title: '예신 준비할 리스트 2', date: '2025.07.24' },
+      { title: '예신 준비할 리스트 3', date: '2025.07.23' },
+    ],
+    done: [{ title: '예신 완료한 리스트', date: '2025.07.21' }],
   },
   groom: {
     done: [{ title: '예랑 완료한 리스트', date: '2025.07.23' }],
