b52fc07aaeef2ecbd35021b0c4f10a61770227a1
vvizi <gmlwl70@gmail.com>
2025-07-22 21:23:59 +0900

    [feat] 날짜순 sorting 구현
    - dropdown을 통한 날짜순 sorting 로직 구현
    - TODO : 카테고리 sorting

--- src/app/home-list/page.tsx
+++ src/app/home-list/page.tsx
@@ -2,42 +2,68 @@
 
 import { ListItem, mockData } from '@/api/homeListMockData';
 import { HomeListItem } from '@/components/molecules/homeList';
-import { HomeListType } from '@/components/molecules/homeList/homeListCard';
+import { HomeListType } from '@/components/molecules/homeList/HomeListCard';
+import {
+  HomeListDropdown,
+  HomeListDropdownType,
+} from '@/components/molecules/homeList/HomeListDropdown';
 import { TabType } from '@/components/molecules/homeList/HomeListTab';
 import { TopBar } from '@/components/molecules/topBar';
 import { useSearchParams } from 'next/navigation';
+import { useMemo, useState } from 'react';
+
+const labelMap: Record<TabType, string> = {
+  together: '함께',
+  bride: '예신',
+  groom: '예랑',
+};
 
 const HomeListPage = () => {
   const searchParams = useSearchParams();
   const type = (searchParams.get('type') ?? 'todo') as HomeListType;
   const role = (searchParams.get('role') ?? 'together') as TabType;
 
-  const items: ListItem[] = mockData[role]?.[type] ?? [];
-
-  const labelMap: Record<TabType, string> = {
-    together: '함께',
-    bride: '예신',
-    groom: '예랑',
-  };
-
   const title =
     type === 'done'
       ? `${labelMap[role]} 완료한 리스트`
-      : `${labelMap[role]} 남은 리스트`;
+      : `${labelMap[role]} 할 리스트`;
+
+  const [sortedOption, setSortedOption] =
+    useState<HomeListDropdownType>('날짜순');
+  const items: ListItem[] = mockData[role]?.[type] ?? [];
+
+  const sortedItems = useMemo(() => {
+    if (sortedOption === '날짜순') {
+      return [...items].sort(
+        (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime(),
+      );
+    }
+    // TODO: 카테고리순 정렬
+    return items;
+  }, [items, sortedOption]);
 
   return (
     <>
       <TopBar className='w-full' />
-      <h2 className='my-4 text-center text-lg font-semibold'>{title}</h2>
-      <div className='flex flex-col items-center space-y-2'>
-        {items.map(({ title, date }) => (
-          <HomeListItem
-            key={title + date}
-            size='small'
-            title={title}
-            date={date}
+      <div className='mx-5'>
+        <h2 className='text-2xl font-semibold text-white'>{title}</h2>
+        <div className='mt-7 flex items-center justify-between'>
+          <span className='text-base font-semibold text-primary-500'>{`총 ${items.length}개`}</span>
+          <HomeListDropdown
+            selectedOption={sortedOption}
+            onSelectAction={setSortedOption}
           />
-        ))}
+        </div>
+        <div className='mt-4 flex flex-col space-y-3'>
+          {sortedItems.map(({ title, date }) => (
+            <HomeListItem
+              key={title + date}
+              size='medium'
+              title={title}
+              date={date}
+            />
+          ))}
+        </div>
       </div>
     </>
   );
